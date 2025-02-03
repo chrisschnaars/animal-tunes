2e49c717d67e1cd7e837c2c04550b800fe8e190c
Chris Schnaars <schnaarschris@gmail.com>
2025-02-03 10:43:05 -0500

    try w/o styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App/index.jsx'
-import './stylesheets/index.css';
+import App from './components/App/App.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
