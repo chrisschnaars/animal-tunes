0bfd3fb1e7ddf8c7b0947ee2110ac84b172f34db
Chris Schnaars <schnaarschris@gmail.com>
2025-02-03 14:08:03 -0500

    use base path?

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     output: 'export', // Outputs a Single-Page Application (SPA).
     distDir: './build', // Changes the build output directory to `./build`.
+    basePath: '/animal-tunes',
   }
    
   export default nextConfig
