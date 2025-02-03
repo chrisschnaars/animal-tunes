ac58793404d91d59a6dbc272ceab02bbf7459549
Chris Schnaars <schnaarschris@gmail.com>
2025-02-03 14:25:57 -0500

    remove height from svg

--- src/app/ui/AnimalCard/index.js
+++ src/app/ui/AnimalCard/index.js
@@ -30,7 +30,7 @@ const AnimalCard = (props) => {
               src={`${process.env.NEXT_PUBLIC_BASE_PATH}/audio/${audio}`}
             >
             </audio>
-            <SVG src={svgMarkup} width={"100%"} height="auto" />
+            <SVG src={svgMarkup} width={"100%"} />
             <kbd className="animal-card__label">{letter.toUpperCase()}</kbd>
         </button>
     );
